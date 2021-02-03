1de2b40aa3179a85dea178e24201d91e996b09a5
Marco Correia <m.correia.m@gmail.com>
2021-02-03 12:07:05 -0300

    Payment, one-to-one association

--- src/main/java/com/marcocorreia/course/config/TestConfig.java
+++ src/main/java/com/marcocorreia/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.marcocorreia.course.entities.Category;
 import com.marcocorreia.course.entities.Order;
 import com.marcocorreia.course.entities.OrderItem;
+import com.marcocorreia.course.entities.Payment;
 import com.marcocorreia.course.entities.Product;
 import com.marcocorreia.course.entities.User;
 import com.marcocorreia.course.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 }
